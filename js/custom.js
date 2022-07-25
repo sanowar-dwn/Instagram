a111cd443dec8a44779584f8d24ba295971fd48b
Sanowar Dewan <sanowardwnofficial@gmail.com>
2022-07-24 22:08:34 -0700

    counter problem solve

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,59 @@
 $(function(){
-    //Counter js here
-    $('.counter-number').counterUp({
-    delay: 10,
-    time: 1000
-    });
+        $('.counter-number').counterUp({
+        delay: 10,
+        time: 1000
+      });
+    //Brand slider js here//
+    $('.review-slider').slick({
+        infinite:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+        responsive: [
+          {
+            breakpoint: 1199.98,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 991.98,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 767.98,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 575,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 319,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });        
+
 });
 
 
